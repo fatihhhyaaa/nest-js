89e53f4accbc95ac017e4397ff9a5484346f5141
fatihhhyaaa <friskiramdhani@gmail.com>
2026-04-18 14:21:48 +0700

    second commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { BcryptService } from 'src/bcrypt/bcrypt.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './guard/role.guard';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, BcryptService, JwtService],
+  providers: [
+    { provide: APP_GUARD, useClass: RolesGuard },
+    AuthService,
+    BcryptService,
+  ],
 })
 export class AuthModule {}
