3f60c50da8df283f47b18af09144e70a31d205c7
fatihhhyaaa <friskiramdhani@gmail.com>
2026-04-26 06:50:09 +0700

    buya

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,31 +10,34 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from 'src/auth/decorators/public.decorators';
+import { Roles } from 'src/auth/decorators/role.decorators';
+
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
+@Public()
   @Post('create')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
-
+@Roles('ADMIN')
   @Get('findall')
   findAll() {
     return this.userService.findAll();
   }
-
+@Public()
   @Get('findone/:id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
-
+@Public()
   @Patch('update/:id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
-
+@Public()
   @Delete('delete/:id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
