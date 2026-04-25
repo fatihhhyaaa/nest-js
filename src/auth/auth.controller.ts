3dc298f8a915222de0a296f637d9e9fb46b34eb6
fatihhhyaaa <friskiramdhani@gmail.com>
2026-04-26 06:58:30 +0700

    yaya

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/authenticate';
+import { Public } from './decorators/public.decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+@Public()
   @Post('login')
   async authenticate(@Body() authDto: AuthDto) {
     return await this.authService.authenticate(authDto);
